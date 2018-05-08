f73ce7305d60b58bc0a68b5f25d399e44d81518e
Simon <26820106+simonhowkins@users.noreply.github.com>
2018-05-08 22:30:47 +0100

    Add copyright messages to all source files

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,23 @@
 #! /usr/bin/env python
 
+# DCC - Digital Command Control command station
+# Copyright (C) 2018 Simon Howkins
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Source for this program is published at https://github.com/simonhowkins/dcc
+
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
